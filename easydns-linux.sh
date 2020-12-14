2b1b4d8f7d98f3a22a68212ae8d228f64894a29d
ssnseawolf <integratebyparts@gmail.com>
2020-12-14 06:52:02 -0600

    Config file update

--- easydns-linux.sh
+++ easydns-linux.sh
@@ -7,20 +7,29 @@ USERNAME="root"
 DOMAIN=""                       # If your home network has a domain name, like home.example.com, enter it here
 DNS_SERVER_1="9.9.9.9"
 DNS_SERVER_2="149.112.112.112"
-DNSMASQ_CONFIG_PATH="/home/ssnseawolf/update-blacklist"
+
+# Download our new dnsmasq config file from our repository before we lose Internet connection
+sudo curl https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/dnsmasq.conf > ~/dnsmasq.conf
+
+# Replace variables in our newly downloaded config file
+sed -i "s/SERVER_IP_ADDR/$SERVER_IP_ADDR" ~/dnsmasq.conf
+sed -i "s/DOMAIN/$DOMAIN" ~/dnsmasq.conf
+sed -i "s/DNS_SERVER_1/$DNS_SERVER_1" ~/dnsmasq.conf
+sed -i "s/DNS_SERVER_2/$DNS_SERVER_2" ~/dnsmasq.conf
+
+# Install dnsmasq
+sudo apt install -y dnsmasq
 
 # systemd-resolved has a stub listener on port 53 by default. It must go
 echo "DNSStubListener=no" | sudo tee -a /etc/systemd/resolved.conf
 sudo systemctl restart systemd-resolved
 
-# Install dnsmasq
-sudo apt install -y dnsmasq
-
-# Download our new dnsmasq config file from our repository
-sudo curl https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/dnsmasq.conf > /etc/dnsmasq.conf
+# Copy our new dnsmasq config file over
+sudo rsync ~/dnsmasq.conf /etc/dnsmasq.conf --remove-source-files 
 
 # Restart dnsmasq after updating the blacklist
 systemctl reload dnsmasq
+
 
 # Download blocklist
 sudo curl https://raw.githubusercontent.com/notracking/hosts-blocklists/master/dnsmasq/dnsmasq.blacklist.txt > /home/dnsmasq.blacklist.txt
