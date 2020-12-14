94c1c780c0eab6c544de5bdc9b5461300ffc708f
ssnseawolf <integratebyparts@gmail.com>
2020-12-14 06:28:16 -0600

    Rearrange downloads

--- easydns-linux.sh
+++ easydns-linux.sh
@@ -12,14 +12,6 @@ DNSMASQ_CONFIG_PATH="/home/ssnseawolf/update-blacklist"
 # We're modifying spooky bits and must run as root
 sudo -i
 
-# Do downloading first
-# Download our new dnsmasq config file from our repository
-sudo curl https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/dnsmasq.conf > /etc/dnsmasq.conf
-
-# Download blocklist
-sudo curl https://raw.githubusercontent.com/notracking/hosts-blocklists/master/dnsmasq/dnsmasq.blacklist.txt > /home/dnsmasq.blacklist.txt
-
-
 # systemd-resolved has a stub listener on port 53 by default. It must go
 sudo echo "DNSStubListener=no" >> /etc/systemd/resolved.conf
 sudo systemctl restart systemd-resolved
@@ -27,15 +19,18 @@ sudo systemctl restart systemd-resolved
 # Install dnsmasq
 sudo apt install -y dnsmasq
 
+# Download our new dnsmasq config file from our repository
+sudo curl https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/dnsmasq.conf > /etc/dnsmasq.conf
 
+# Restart dnsmasq after updating the blacklist
+systemctl reload dnsmasq
+
+# Download blocklist
+sudo curl https://raw.githubusercontent.com/notracking/hosts-blocklists/master/dnsmasq/dnsmasq.blacklist.txt > /home/dnsmasq.blacklist.txt
 
 # Run this file as a cron job
 # Every other day cron: 0 0 2-30/2 * * /home/update-blacklist.sh
 
-
 # Create a cron job to update adlist every two days
 # Formatted to look pretty in the cron list
 #CRONJOB=" 0 0 2-30/2 * * root    /home/update-blacklist.sh"
-
-# Restart dnsmasq after updating the blacklist
-systemctl reload dnsmasq
