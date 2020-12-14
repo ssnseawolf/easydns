2b84d306ef9d97208f8979c72d845125d25f8bb2
ssnseawolf <integratebyparts@gmail.com>
2020-12-14 07:54:34 -0600

    Fix netmask

--- easydns-linux.sh
+++ easydns-linux.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # User configuration
-SERVER_IP_ADDR="192.168.0.4/16"        # IP address to use for your server. Typically 192.168.0.x or 192.168.1.x
+SERVER_IP_ADDR="192.168.0.4"        # IP address to use for your server. Typically 192.168.0.x or 192.168.1.x
+SERVER_IP_NETMASK_CIDR="16"
 GATEWAY_IP_ADDR="192.168.0.1"
 DOMAIN="None"                       # If your home network has a domain name, like home.example.com, enter it here
 DNS_SERVER_1="9.9.9.9"
@@ -9,7 +10,7 @@ DNS_SERVER_2="149.112.112.112"
 
 # Download our new dnsmasq config file from our repository before we lose Internet connection
 curl https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/dnsmasq.conf > ~/dnsmasq.conf
-curl https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/dnsmasq.conf > ~/netplan.yaml
+curl https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/netplan.yaml > ~/netplan.yaml
 
 # Replace variables in our newly downloaded config file
 sed -i "s/SERVER_IP_ADDR/$SERVER_IP_ADDR/" ~/dnsmasq.conf
@@ -20,6 +21,7 @@ sed -i "s/DNS_SERVER_2/$DNS_SERVER_2/" ~/dnsmasq.conf
 
 # Replace variables in our newly downloaded netplan file
 sed -i "s/SERVER_IP_ADDR/$SERVER_IP_ADDR/" ~/netplan.yaml
+sed -i "s/SERVER_IP_NETMASK_CIDR/$SERVER_IP_NETMASK_CIDR/" ~/netplan.yaml
 sed -i "s/GATEWAY_IP_ADDR/$GATEWAY_IP_ADDR/" ~/netplan.yaml
 
 # Install dnsmasq
@@ -27,7 +29,8 @@ sudo apt install -y dnsmasq
 
 # Apply our config and netplan files
 sudo rsync ~/dnsmasq.conf /etc/dnsmasq.conf --remove-source-files
-sudo rsync ~/netplan.conf /etc/netplan/*.yaml --remove-source-files 
+sudo rm /etc/netplan/*.yaml
+sudo rsync ~/netplan.yaml /etc/netplan/netplan.yaml
 sudo netplan apply
 
 # systemd-resolved has a stub listener on port 53 by default. It must go
