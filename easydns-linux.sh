89b602ea6fe61d9ee25215f5a863d72b2616faec
ssnseawolf <integratebyparts@gmail.com>
2020-12-20 09:51:14 -0600

    Cron logging

--- easydns-linux.sh
+++ easydns-linux.sh
@@ -6,14 +6,17 @@ SERVER_IP_NETMASK_CIDR=""
 GATEWAY_IP_ADDR="1"
 DNS_SERVER_1="9.9.9.9"
 DNS_SERVER_2="149.112.112.112"
+HOSTNAME=""
 
 # Request default IP
-read -p "Static IP to set. Default is 192.168.1.10: " SERVER_IP_ADDR
-read -p "CIDR subnet (no slash, i.e. 16 or 24). Default is 24: " SERVER_IP_NETMASK_CIDR
-read -p "Gateway IP. Default is 192.168.1.1: " GATEWAY_IP_ADDR
+read -p "Static IP [192.168.1.10] " SERVER_IP_ADDR
+read -p "CIDR subnet (no slash, i.e. 16 or 24) [24]: " SERVER_IP_NETMASK_CIDR
+read -p "Gateway IP. []192.168.1.1] " GATEWAY_IP_ADDR
+read -p "Hostname [dns]: " HOSTNAME
 SERVER_IP_ADDR=${SERVER_IP_ADDR:-192.168.1.10}
 SERVER_IP_NETMASK_CIDR=${SERVER_IP_NETMASK_CIDR:-24}
 GATEWAY_IP_ADDR=${GATEWAY_IP_ADDR:-192.168.1.1}
+HOSTNAME=${HOSTNAME:-dns}
 
 # Download our new dnsmasq config file from our repository before we lose Internet connection
 curl https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/dnsmasq.conf > ~/dnsmasq.conf
@@ -33,14 +36,10 @@ sed -i "s/GATEWAY_IP_ADDR/$GATEWAY_IP_ADDR/" ~/netplan.yaml
 BLACKLIST_URL="https://raw.githubusercontent.com/notracking/hosts-blocklists/master/dnsmasq/dnsmasq.blacklist.txt"
 curl $BLACKLIST_URL | tee /etc/dnsmasq.blacklist.txt > /dev/null
 
-# Cut our Internet as we switch off systemd-resolved
-#sudo systemctl disable systemd-resolved
-dnf install -y dnsmasq
-
-# Make sure server is updated and install bind-utils (optional, but nice for dig)
+# Make sure server is updated
 dnf -y upgrade
-dnf install -y bind-utils
-dnf install -y nano
+dnf install -y dnsmasq      # DNS server
+dnf install -y bind-utils   # For dig utility, not necessary
 
 # Enable automatic updates with automatic reboot
 dnf install -y dnf-automatic
@@ -52,10 +51,12 @@ nmcli connection modify eth0 IPv4.address $SERVER_IP_ADDR/$SERVER_IP_NETMASK_CID
 nmcli connection modify enp1s0 IPv4.gateway $GATEWAY_IP_ADDR
 nmcli connection modify enp1s0 IPv4.method manual
 
-# Update ablock list as a cronjob
-# Create a cron job to update adlist every day at midnight with a 1 hour random offset
-CRONJOB="0 1 * * * root    perl -le 'sleep rand 3600' && curl $BLACKLIST_URL | tee /etc/dnsmasq.blacklist.txt"
-echo "$CRONJOB" | sudo tee -a /etc/crontab
+# Update adblock list daily
+CRONJOB="1 0 1 * * root    perl -le 'sleep rand 3600' && curl $BLACKLIST_URL | tee /etc/dnsmasq.blacklist.txt && echo 'date Updated adblock list' >> /var/log/adblock-update.log"
+crontab -l > mycron
+echo "$CRONJOB" >> mycron
+crontab mycron
+rm mycron
 
 # We lazy
 reboot
