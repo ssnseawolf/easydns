852f5238fa71e49c085dbfcafd3c732c79349f84
ssnseawolf <integratebyparts@gmail.com>
2020-12-21 12:08:34 -0600

    Project rename

--- easydns-linux.sh
+++ easydns-linux.sh
@@ -19,8 +19,8 @@ GATEWAY_IP_ADDR=${GATEWAY_IP_ADDR:-192.168.1.1}
 HOSTNAME=${HOSTNAME:-dns}
 
 # Download our new dnsmasq config file from our repository before we lose Internet connection
-curl https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/dnsmasq.conf > ~/dnsmasq.conf
-curl https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/netplan.yaml > ~/netplan.yaml
+curl https://raw.githubusercontent.com/ssnseawolf/easydns/master/dnsmasq.conf > ~/dnsmasq.conf
+curl https://raw.githubusercontent.com/ssnseawolf/easydns/master/netplan.yaml > ~/netplan.yaml
 
 # Replace variables in our newly downloaded config file
 sed -i "s/SERVER_IP_ADDR/$SERVER_IP_ADDR/" ~/dnsmasq.conf
@@ -43,7 +43,7 @@ dnf install -y bind-utils   # For dig utility, not necessary
 
 # Enable automatic updates with automatic reboot
 dnf install -y dnf-automatic
-AUTOMATIC_UPDATE_URL="https://raw.githubusercontent.com/ssnseawolf/easydns-linux/master/automatic.conf"
+AUTOMATIC_UPDATE_URL="https://raw.githubusercontent.com/ssnseawolf/easydns/master/automatic.conf"
 curl $AUTOMATIC_UPDATE_URL | tee /etc/dnf/automatic.conf
 
 # Apply our config and netplan files
