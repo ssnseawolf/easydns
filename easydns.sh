ff0e819560bb598a97cb639404d66cef0633c2a6
ssnseawolf <integratebyparts@gmail.com>
2020-12-22 10:26:11 -0600

    Refactor conf file in script

--- easydns.sh
+++ easydns.sh
@@ -19,13 +19,6 @@ SERVER_IP_NETMASK_CIDR=${SERVER_IP_NETMASK_CIDR:-24}
 GATEWAY_IP_ADDR=${GATEWAY_IP_ADDR:-192.168.1.1}
 HOSTNAME=${HOSTNAME:-dns}
 
-# Replace variables in our newly downloaded config file
-sed -i "s/SERVER_IP_ADDR/$SERVER_IP_ADDR/" ~/dnsmasq.conf
-sed -i "s/HOSTNAME/$HOSTNAME/" ~/dnsmasq.conf
-sed -i "s/domain=DOMAIN//" ~/dnsmasq.conf
-sed -i "s/DNS_SERVER_1/$DNS_SERVER_1/" ~/dnsmasq.conf
-sed -i "s/DNS_SERVER_2/$DNS_SERVER_2/" ~/dnsmasq.conf
-
 # Download blocklist for first time
 # Uncomment for dnsmasq >=2.80 (RHEL 9?)
 #BLACKLIST_URLS="https://raw.githubusercontent.com/notracking/hosts-blocklists/master/dnsmasq/dnsmasq.blacklist.txt"
@@ -49,14 +42,21 @@ dnf install -y dnsmasq      # DNS server
 systemctl enable dnsmasq    # Enable dnsmasq on startup
 $DNSMASQ_CONF =https://raw.githubusercontent.com/ssnseawolf/easydns/master/dnsmasq.conf
 curl $DNSMASQ_CONF | tee /etc/dnsmasq.conf > /dev/null
+sed -i "s/SERVER_IP_ADDR/$SERVER_IP_ADDR/" /etc/dnsmasq.conf
+sed -i "s/HOSTNAME/$HOSTNAME/" /etc/dnsmasq.conf
+sed -i "s/domain=DOMAIN//" /etc/dnsmasq.conf
+sed -i "s/DNS_SERVER_1/$DNS_SERVER_1/" /etc/dnsmasq.conf
+sed -i "s/DNS_SERVER_2/$DNS_SERVER_2/" /etc/dnsmasq.conf
 
 # Punch a hole through the firewall for DNS
 firewall-cmd --add-service=dns --permanent
 
 # Enable automatic system updates with automatic reboot
 dnf install -y dnf-automatic
+
+# Replace variables in our newly downloaded config file
 AUTOMATIC_UPDATE_URL="https://raw.githubusercontent.com/ssnseawolf/easydns/master/automatic.conf"
-curl $AUTOMATIC_UPDATE_URL | tee /etc/dnf/automatic.conf
+curl $AUTOMATIC_UPDATE_URL | tee /etc/cron.daily/automatic.conf
 
 # Configure our network settings
 nmcli connection modify eth0 IPv4.address $SERVER_IP_ADDR/$SERVER_IP_NETMASK_CIDR
