245cd57bd3c5a0723a672e53e55146afad9847cd
ssnseawolf <integratebyparts@gmail.com>
2020-12-22 09:28:05 -0600

    update script comments

--- pre-2.80-dnsmasq-cron-update.sh
+++ pre-2.80-dnsmasq-cron-update.sh
@@ -11,10 +11,10 @@
 # HOWTO
 # For Debian based systems
 #  - Install curl: sudo apt install curl
-#  - Download this file to: /etc/cron.daily/notracking_update
-#  - Mark it as executable: sudo chmod +x /etc/cron.daily/notracking_update
-#  - Update the CONFIG section in this script: sudo nano /etc/cron.daily/notracking_update
-#  - Do a test run: sudo /etc/cron.daily/notracking_update
+#  - Download this file to: /etc/cron.daily/adblocking_update
+#  - Mark it as executable: sudo chmod +x /etc/cron.daily/adblocking_update
+#  - Update the CONFIG section in this script: sudo nano /etc/cron.daily/adblocking_update
+#  - Do a test run: sudo /etc/cron.daily/adblocking_update
 #
 # WHITELISTING
 # Any line in the blocklist that contains one of the lines in the whitelist will be automatically removed.
